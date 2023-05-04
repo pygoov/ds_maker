d7a048bd747e84dd9e2e1932f2f50334033fb8bc
goov <goov@ocas.ai>
2023-05-04 14:52:00 +0500

    fix

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import argparse
 
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ContentType
+from aiogram.types import ContentType, ParseMode
+from typing import List
+
 from openai_client import OpenAIClient
 
 parser = argparse.ArgumentParser()
@@ -16,26 +18,40 @@ dp = Dispatcher(bot)
 with open("./prompt.txt", "r", encoding="utf-8") as f:
     PROMPT = f.read()
 
+HELP_MSG = (
+    "Пожалуйста, напишите мне две строки в сообщении.\n"
+    "В первой строке описание класса.\n"
+    "Во второй строке, пример вопроса который яперефразурую.\n\n"
+    "пример сообщений:\n\n"
+    "льготный возраст ребёнка, для заселения без платы\n"
+    "сколько лет ребёнку должно быть, чтобы нас бесплатно заселили?\n"
+)
 
-async def make_new_line(class_line: str, text_line: str) -> str:
+
+async def make_new_line(class_line: str,
+                        text_line: str,
+                        temperature: float,
+                        presence_penalty: float,
+                        n: int) -> List[str]:
     p = (
         PROMPT
         .replace("<CLASS>", class_line)
         .replace("<TEXT>", text_line)
-    )
+    )    
     result = await oai_client.completions(
         prompt=p,
-        temperature=0.8,
-        presence_penalty=1,
+        temperature=temperature,
+        presence_penalty=presence_penalty,
         max_tokens=500,
+        n=n,
         stop="\n"
     )
-    return result
+    return [x["text"] for x in result if "text" in x]
 
 
 @dp.message_handler(commands=['start'])
 async def start_handler(message: types.Message):
-    await message.answer("Приветствую!")
+    await message.answer(HELP_MSG)
 
 
 @dp.message_handler(content_types=ContentType.TEXT)
@@ -43,36 +59,37 @@ async def message_handler(message: types.Message):
     text = message.text
     sp = text.split("\n")
     if len(sp) != 2:
-        await message.answer(
-            "Пожалуйста, напишите мне две строки в сообщении.\n"
-            "В первой строке описание класса.\n"
-            "Во второй строке, пример вопроса который яперефразурую.\n\n"
-            "пример сообщений:\n"
-            "```\n"
-            "льготный возраст ребёнка, для заселения без платы\n"
-            "сколько лет ребёнку должно быть, чтобы нас бесплатно заселили?\n"
-            "```"
-        )
+        await message.answer(HELP_MSG)
         return
 
     class_line = sp[0].strip()
     text_line = sp[1].strip()
 
     if not class_line or not text_line:
-        await message.answer(
-            "Пожалуйста, напишите мне две строки в сообщении.\n"
-            "В первой строке описание класса.\n"
-            "Во второй строке, пример вопроса который яперефразурую.\n\n"
-            "пример сообщений:\n"
-            "```\n"
-            "получение паспорта\n"
-            "как получить паспорт?\n"
-            "```"
-        )
+        await message.answer(HELP_MSG)
         return
 
-    line = await make_new_line(class_line, text_line)
-    await message.answer(line)
+    print(f"run genegate - {message.from_user}")
+    print(f'{class_line=}')
+    print(f'{text_line=}')
+
+    for temperature in [0.25, 0.75, 1, 1.25]:
+        for presence_penalty in [-0.5, 0, 0.5]:
+            try:
+                line = (await make_new_line(
+                    class_line,
+                    text_line,
+                    temperature,
+                    presence_penalty,
+                    1
+                ))[0]
+                # await message.answer(line)
+                await message.answer(
+                    f'=== [t:{temperature}|p:{presence_penalty}] ===\n{line}'
+                )
+            except Exception as e:
+                print(e)
+            # line = "\n".join(lines)
 
 
 if __name__ == '__main__':
--- openai_client.py
+++ openai_client.py
@@ -1,6 +1,6 @@
 import aiohttp
 
-from typing import Optional
+from typing import Optional, List
 
 
 class OpenAIClient:
@@ -14,6 +14,17 @@ class OpenAIClient:
         keargs["headers"] = headers
         return aiohttp.request(*args, **keargs)
 
+    async def get_balance(self) -> dict:
+        async with self._request(
+            "GET", "https://api.openai.com/dashboard/billing/subscription",
+        ) as rsp:
+            if rsp.status != 200:
+                text = await rsp.read()
+                raise Exception(f'request failed[statue={rsp.status}]: {text}')
+            data = await rsp.json()
+            assert isinstance(data, dict)
+            return data
+
     async def completions(self,
                           prompt: str,
                           model: str = "text-davinci-003",
@@ -21,7 +32,9 @@ class OpenAIClient:
                           temperature: float = 0.5,
                           presence_penalty: float = 0.5,
                           top_p: float = 1,
-                          stop: Optional[str] = None) -> str:
+                          n: int = 1,
+                          stop: Optional[str] = None) -> List[dict]:
+        print('start completions')
         async with self._request(
             "POST", "https://api.openai.com/v1/completions",
             json={
@@ -31,11 +44,15 @@ class OpenAIClient:
                 "temperature": temperature,
                 "presence_penalty": presence_penalty,
                 "top_p": top_p,
-                "n": 1,
+                "n": n,
                 "stop": stop,
                 "stream": False,  # TODO: may be write
             }
         ) as rsp:
+            print('completions end')
+            if rsp.status != 200:
+                text = await rsp.read()
+                raise Exception(f'request failed[statue={rsp.status}]: {text}')
             data = await rsp.json()
-            print(data)
-        return data["choices"][0]["text"]
+
+        return data["choices"]
